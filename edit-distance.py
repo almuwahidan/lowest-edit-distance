6bf872d6148c6a351b6f646f7c6525e22c9d5f2b
Ibrahim Al Muwahidan <almuwahidan@gmail.com>
2022-06-23 09:43:53 +0700

    Added empty functions

---
+++ edit-distance.py
@@ -0,0 +1,30 @@
+
+def edit_distance(word, reference):
+    """Returns edit distance (integer) of a word against a reference.
+
+    Parameters
+    word -- a word to be evaluated
+    reference -- reference against the word
+    """
+    edit_dist = None
+
+    return edit_dist
+
+
+def main(eval_words, ref_words):
+    """Returns a list of words with their lowest edit distance.
+
+    Parameters:
+    words_eval -- words being evaluated.
+    references -- words with references.
+    """
+    print("Hello world!")
+    
+    return evaluated_words
+
+
+if __name__ == "__main__":
+    eval_words = None
+    ref_words = None
+
+    evaluated_words = main(eval_words, ref_words)
